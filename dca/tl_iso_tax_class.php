296b1594d038df166d9368c38b5bac1c3b33fbc6
Monique Hahnefeld <info@monique-hahnefeld.de>
2014-05-12 19:07:20 +0200

    Update tl_iso_tax_class.php

--- dca/tl_iso_tax_class.php
+++ dca/tl_iso_tax_class.php
@@ -45,6 +45,7 @@ $GLOBALS['TL_DCA']['tl_iso_tax_class']['fields']['germanize_price'] = array
     'options'       => array('gross', 'net'),
     'reference'     => &$GLOBALS['TL_LANG']['tl_iso_tax_class'],
     'eval'          => array('mandatory'=>true, 'tl_class'=>'w50'),
+    'sgl'       => "varchar(5) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_iso_tax_class']['fields']['germanize_rate'] = array
@@ -53,4 +54,5 @@ $GLOBALS['TL_DCA']['tl_iso_tax_class']['fields']['germanize_rate'] = array
     'exclude'       => true,
     'inputType'     => 'text',
     'eval'          => array('rgxp'=>digit, 'mandatory'=>true, 'tl_class'=>'w50'),
+    'sgl'       => "decimal(12,2) NOT NULL default '0.00"
 );
