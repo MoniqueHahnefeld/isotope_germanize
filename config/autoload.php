726b05b9e975fbdd6bf3800cdd65f1923b803fd3
Monique Hahnefeld <info@monique-hahnefeld.de>
2014-05-12 19:25:38 +0200

    Update autoload.php

--- config/autoload.php
+++ config/autoload.php
@@ -1,1 +1,33 @@
-//autoload
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   isotope_germize
+ * @author    Monique Hahnefeld
+ * @license   LGPL
+ * @copyright 2014 Monique Hahnefeld
+ */
+
+
+/**
+ * Register the namespaces
+
+ClassLoader::addNamespaces(array
+(
+	'Contao',
+	'MHAHNEFELD\FTC',
+));
+ */
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	// Classes
+	
+	'IXR_Library' => 'system/modules/isotope_germanize/classes/IXR_Library.php',
+	'IsotopeGermanize' => 'system/modules/isotope_germanize/classes/IsotopeGermanize.php'
+	
+	
