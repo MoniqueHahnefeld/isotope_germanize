f438c6cab9ff1f580a43dfced4c4908e2b9ca0b6
Monique Hahnefeld <info@monique-hahnefeld.de>
2014-05-12 19:05:14 +0200

    Update and rename tl_iso_addresses.php to tl_iso_address.php

--- dca/tl_iso_address.php
+++ dca/tl_iso_address.php
@@ -18,14 +18,14 @@
  * Modify palettes
  */
 
-$GLOBALS['TL_DCA']['tl_iso_addresses']['palettes']['default'] = str_replace('vat_no','vat_no,vat_no_ok',$GLOBALS['TL_DCA']['tl_iso_addresses']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_iso_address']['palettes']['default'] = str_replace('vat_no','vat_no,vat_no_ok',$GLOBALS['TL_DCA']['tl_iso_address']['palettes']['default']);
 
 
 /**
  * Add fields
  */
 
-$GLOBALS['TL_DCA']['tl_iso_addresses']['fields']['vat_no_ok'] = array
+$GLOBALS['TL_DCA']['tl_iso_address']['fields']['vat_no_ok'] = array
 (
 	'label'					=> &$GLOBALS['TL_LANG']['tl_iso_addresses']['vat_no_ok'],
 	'exclude'				=> true,
@@ -34,5 +34,6 @@ $GLOBALS['TL_DCA']['tl_iso_addresses']['fields']['vat_no_ok'] = array
 	'options'				=> array('nok', 'nok_invalid', 'nok_simple', 'nok_qualified', 'ok_qualified', 'ok_manual'),
 	'reference'				=> &$GLOBALS['TL_LANG']['tl_iso_addresses'],
 	'default'				=> 'nok',
-	'eval'					=> array('feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50 m12')
+	'eval'					=> array('feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50 m12'),
+	'sql'		=>"varchar(16) NOT NULL default 'nok'"
 );
