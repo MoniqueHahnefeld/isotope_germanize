7dfc7715256cc2ebbec26121f25e1f75ab00cdef
Monique Hahnefeld <info@monique-hahnefeld.de>
2014-05-18 23:56:38 +0200

    Update default.php

--- languages/en/default.php
+++ languages/en/default.php
@@ -27,54 +27,54 @@ $GLOBALS['TL_LANG']['MSC']['confirmOrder']                          = 'Buy Now';
  * Notes at the products, in the cart etc
  */
  
-$GLOBALS['TL_LANG']['iso_germanize']['priceNotes']['taxfree']       = 'TaxFree';
-$GLOBALS['TL_LANG']['iso_germanize']['priceNotes']['linkname']       = 'Versand';
-$GLOBALS['TL_LANG']['iso_germanize']['priceNotes']['shipping']       = 'plus ';
-$GLOBALS['TL_LANG']['iso_germanize']['priceNotes']['noShipping']     = ', no Shipping';
-$GLOBALS['TL_LANG']['iso_germanize']['priceNotes']['taxfree_shipping']   = 'plus  <a>Shipping</a>';
-$GLOBALS['TL_LANG']['iso_germanize']['priceNotes']['taxfree_noShipping'] = 'no Shipping';
+$GLOBALS['TL_LANG']['iso_legal']['priceNotes']['taxfree']       = 'TaxFree';
+$GLOBALS['TL_LANG']['iso_legal']['priceNotes']['linkname']       = 'Versand';
+$GLOBALS['TL_LANG']['iso_legal']['priceNotes']['shipping']       = 'plus ';
+$GLOBALS['TL_LANG']['iso_legal']['priceNotes']['noShipping']     = ', no Shipping';
+$GLOBALS['TL_LANG']['iso_legal']['priceNotes']['taxfree_shipping']   = 'plus  <a>Shipping</a>';
+$GLOBALS['TL_LANG']['iso_legal']['priceNotes']['taxfree_noShipping'] = 'no Shipping';
 
-$GLOBALS['TL_LANG']['iso_germanize']['notes']['nonEuGuest']        = 'Die Preise werden unabhängig vom Lieferland %s inkl. MwSt. angezeigt. Bei Lieferung in nicht-EU-Länder wird diese in der Bestellübersicht nicht berücksichtigt.';
-$GLOBALS['TL_LANG']['iso_germanize']['notes']['nonEu']             = 'Als Lieferung an einen Leistungsempfänger in dem nicht-EU-Land %s ist der Umsatz nicht steuerbar. Es wird daher keine MwSt. berechnet.';
-$GLOBALS['TL_LANG']['iso_germanize']['notes']['confirmedVatNo']    = 'Die USt.-Id %s ist bestätigt. Der Leistungsempfänger entspricht der Lieferadresse, daher wird bei dieser innergemeinschaftlichen Leistung keine MwSt in Rechnung gestellt.';
-$GLOBALS['TL_LANG']['iso_germanize']['notes']['unconfirmedVatNo']  = 'Die USt.-Id %s wurde bisher leider noch nicht bestätigt. Daher wird unabhängig davon in das Lieferland %s inkl. MwSt. berechnet.';
-$GLOBALS['TL_LANG']['iso_germanize']['notes']['noVatNo']           = 'Die Preise werden unabhängig vom Lieferland %s inkl. MwSt. angezeigt. Geben Sie bei der Bestellung eine gültige USt.-Id. an, wird entsprechend einer innergemeinschaftlichen Leistung keine MwSt in Rechnung gestellt.';
+$GLOBALS['TL_LANG']['iso_legal']['notes']['nonEuGuest']        = 'Die Preise werden unabhängig vom Lieferland %s inkl. MwSt. angezeigt. Bei Lieferung in nicht-EU-Länder wird diese in der Bestellübersicht nicht berücksichtigt.';
+$GLOBALS['TL_LANG']['iso_legal']['notes']['nonEu']             = 'Als Lieferung an einen Leistungsempfänger in dem nicht-EU-Land %s ist der Umsatz nicht steuerbar. Es wird daher keine MwSt. berechnet.';
+$GLOBALS['TL_LANG']['iso_legal']['notes']['confirmedVatNo']    = 'Die USt.-Id %s ist bestätigt. Der Leistungsempfänger entspricht der Lieferadresse, daher wird bei dieser innergemeinschaftlichen Leistung keine MwSt in Rechnung gestellt.';
+$GLOBALS['TL_LANG']['iso_legal']['notes']['unconfirmedVatNo']  = 'Die USt.-Id %s wurde bisher leider noch nicht bestätigt. Daher wird unabhängig davon in das Lieferland %s inkl. MwSt. berechnet.';
+$GLOBALS['TL_LANG']['iso_legal']['notes']['noVatNo']           = 'Die Preise werden unabhängig vom Lieferland %s inkl. MwSt. angezeigt. Geben Sie bei der Bestellung eine gültige USt.-Id. an, wird entsprechend einer innergemeinschaftlichen Leistung keine MwSt in Rechnung gestellt.';
 
 /**
  * VAT-Id check legends
  */
 
-$GLOBALS['TL_LANG']['iso_germanize']['error']['vat_no']            = 'Fehler in der USt.-ID';
-$GLOBALS['TL_LANG']['iso_germanize']['error']['own_vat_no']        = 'Fehler in der eigenen USt.-ID';
-$GLOBALS['TL_LANG']['iso_germanize']['error']['server']            = 'Server nicht erreichbar';
-$GLOBALS['TL_LANG']['iso_germanize']['error']['general']           = 'Fehler in der Abfrage, siehe Fehlercodes';
+$GLOBALS['TL_LANG']['iso_legal']['error']['vat_no']            = 'Fehler in der USt.-ID';
+$GLOBALS['TL_LANG']['iso_legal']['error']['own_vat_no']        = 'Fehler in der eigenen USt.-ID';
+$GLOBALS['TL_LANG']['iso_legal']['error']['server']            = 'Server nicht erreichbar';
+$GLOBALS['TL_LANG']['iso_legal']['error']['general']           = 'Fehler in der Abfrage, siehe Fehlercodes';
 
-$GLOBALS['TL_LANG']['iso_germanize']['vat_no_confirmed']           = 'bestätigt';
-$GLOBALS['TL_LANG']['iso_germanize']['vat_no_notconfirmed']        = 'USt.-ID nicht bestätigt';
+$GLOBALS['TL_LANG']['iso_legal']['vat_no_confirmed']           = 'bestätigt';
+$GLOBALS['TL_LANG']['iso_legal']['vat_no_notconfirmed']        = 'USt.-ID nicht bestätigt';
 
-$GLOBALS['TL_LANG']['iso_germanize']['bff']['A']                   = 'stimmt überein';
-$GLOBALS['TL_LANG']['iso_germanize']['bff']['B']                   = 'stimmt nicht überein';
-$GLOBALS['TL_LANG']['iso_germanize']['bff']['C']                   = 'nicht angefragt';
-$GLOBALS['TL_LANG']['iso_germanize']['bff']['D']                   = 'vom EU-Mitgliedsstaat nicht mitgeteilt';
+$GLOBALS['TL_LANG']['iso_legal']['bff']['A']                   = 'stimmt überein';
+$GLOBALS['TL_LANG']['iso_legal']['bff']['B']                   = 'stimmt nicht überein';
+$GLOBALS['TL_LANG']['iso_legal']['bff']['C']                   = 'nicht angefragt';
+$GLOBALS['TL_LANG']['iso_legal']['bff']['D']                   = 'vom EU-Mitgliedsstaat nicht mitgeteilt';
 
 
 /**
  * Notification mails
  */
 
-$GLOBALS['TL_LANG']['iso_germanize']['nok']            = 'nicht freigeschaltet - ungeprüft';
-$GLOBALS['TL_LANG']['iso_germanize']['nok_invalid']    = 'nicht freigeschaltet - nicht verfizierbar';
-$GLOBALS['TL_LANG']['iso_germanize']['nok_simple']     = 'nicht freigeschaltet - gültig';
-$GLOBALS['TL_LANG']['iso_germanize']['nok_qualified']  = 'nicht freigeschaltet - verifiziert';
-$GLOBALS['TL_LANG']['iso_germanize']['ok_qualified']   = 'automatisch freigeschaltet';
-$GLOBALS['TL_LANG']['iso_germanize']['ok_manual']      = 'manuell freigeschaltet';
+$GLOBALS['TL_LANG']['iso_legal']['nok']            = 'nicht freigeschaltet - ungeprüft';
+$GLOBALS['TL_LANG']['iso_legal']['nok_invalid']    = 'nicht freigeschaltet - nicht verfizierbar';
+$GLOBALS['TL_LANG']['iso_legal']['nok_simple']     = 'nicht freigeschaltet - gültig';
+$GLOBALS['TL_LANG']['iso_legal']['nok_qualified']  = 'nicht freigeschaltet - verifiziert';
+$GLOBALS['TL_LANG']['iso_legal']['ok_qualified']   = 'automatisch freigeschaltet';
+$GLOBALS['TL_LANG']['iso_legal']['ok_manual']      = 'manuell freigeschaltet';
 
-$GLOBALS['TL_LANG']['iso_germanize']['guest_order']                 = 'Gast-Bestellung';
-$GLOBALS['TL_LANG']['iso_germanize']['inactive']                    = 'INAKTIV: ';
+$GLOBALS['TL_LANG']['iso_legal']['guest_order']                 = 'Gast-Bestellung';
+$GLOBALS['TL_LANG']['iso_legal']['inactive']                    = 'INAKTIV: ';
 
-$GLOBALS['TL_LANG']['iso_germanize']['mail_verfication_subject']   = '##inactive##USt.-ID Verifizierung ##vat_no## (##company##)';
+$GLOBALS['TL_LANG']['iso_legal']['mail_verfication_subject']   = '##inactive##USt.-ID Verifizierung ##vat_no## (##company##)';
 
-$GLOBALS['TL_LANG']['iso_germanize']['mail_verfication_text']      = '
+$GLOBALS['TL_LANG']['iso_legal']['mail_verfication_text']      = '
 Automatisierte USt.-ID-Prüfung
 =======================================
 
@@ -110,9 +110,9 @@ Mitglieds-ID : ##member_id##
 Adressen-ID  : ##address_id##
 ';
 
-$GLOBALS['TL_LANG']['iso_germanize']['mail_reminder_subject']      = 'Fehlende USt.-ID Verifizierung ##vat_no## (##company##)';
+$GLOBALS['TL_LANG']['iso_legal']['mail_reminder_subject']      = 'Fehlende USt.-ID Verifizierung ##vat_no## (##company##)';
 
-$GLOBALS['TL_LANG']['iso_germanize']['mail_reminder_text']         = '
+$GLOBALS['TL_LANG']['iso_legal']['mail_reminder_text']         = '
 Fehlerhafte USt.-ID-Prüfung
 =======================================
 
@@ -144,3 +144,4 @@ Straße       : ##check_street##
 PLZ          : ##check_postal##
 Ort          : ##check_city##
 ';
+?>
